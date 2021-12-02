0c99bff22b05dc44c75186f913f2935ee8354b38
Freeman Bird <freemanbird@gmail.com>
2021-12-02 09:49:18 -0700

    challenge complete

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -1,1 +1,78 @@
-// Write your solution below:
+const tshirtSorter = (str) => {
+    let s = '';
+    let m = '';
+    let l = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 's') {
+            s += 's';
+        } else if (str[i] === 'm') {
+            m += 'm';
+        } else /* if (str[i] === 'l') */{
+            l += 'l';
+        }
+    }
+
+    return s + m + l;
+    // sml 'ssss' + 'mmmmmmmm' + 'lllllll'
+}
+
+
+
+
+
+
+console.log(tshirtSorter('mmmllsmmlsmlmslmslmmmm'));
+//                        ssssmmmmmmmmmmmmllllll
+
+
+
+
+
+
+
+
+
+
+
+
+
+// i++ i moves by one each time
+// i += 2, then i moves by two each time
+
+// 'mmmllsmmlsmlmslmslmmmm'
+//  012345^
+// i === 6;
+// currentChar = string[i]
+// if (currentChar === m)
+// if (currentChar === s)
+// if (currentChar === l)
+// string[i] === l
+// m = 'mmmm'
+// s = 's'
+// l = 'll'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
